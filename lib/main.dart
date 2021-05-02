045ff95fc6bee5bfb5e487ebc2bdb53ef94cd741
Minh Nguyen <tmnguyen1403@gmail.com>
2021-05-02 09:43:16 -0500

    Display student info

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,23 @@ class _UIState extends State<UI> {
         '/': (context) => Login(),
         '/admin': (context) => Admin(),
         '/student': (context) => StudentRole(),
-      }
+      },
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+
+        // Define the default font family.
+        fontFamily: 'Georgia',
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline2: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
     );
   }
 }
--- lib/studentview.dart
+++ lib/studentview.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'Model/student.dart';
 
@@ -8,7 +10,17 @@ class StudentView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(children: <Widget>[
-      Text(student.email),
+      ElevatedButton(
+        onPressed: () => {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(
+              builder: (context) => StudentInfo(student: student,), 
+            ) 
+          ) 
+        }, 
+        child: Text(student.email)
+      )
     ],);
   }
 }
@@ -16,16 +28,58 @@ class StudentView extends StatelessWidget {
 class StudentInfo extends StatelessWidget {
   final Student student;
   StudentInfo({required this.student});
+  @override
+  Widget build(BuildContext context) {
+    final String dob = student.dob.substring(0, 10);
+    return Scaffold(
+      appBar: AppBar(title: Text("Student Info"),),
+      body: Column(children: <Widget>[
+        RowInfo(label: "ID", content: student.id.toString(),),
+        RowInfo(label: "email", content: student.email),
+        RowInfo(label: "firstName", content: student.firstName,),
+        RowInfo(label: "lastName", content: student.lastName,),
+        RowInfo(label: "sex", content: student.sex),
+        RowInfo(label: "dob", content: dob),
+      ],),
+    );
+      
+  }
+}
+
+class RowInfo extends StatelessWidget {
+  final String label;
+  final String content;
+
+  RowInfo({required this.label, required this.content});
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: <Widget>[
-      Text(student.email),
-      Text(student.id.toString()),
-      Text(student.firstName),
-      Text(student.lastName),
-      Text(student.sex),
-      Text(student.dob),
-    ],);
+     return Row(
+       //mainAxisAlignment: MainAxisAlignment.center,
+       children: [
+        Expanded(
+          flex: 1,
+          child: Text(
+            label,
+            textAlign: TextAlign.center,
+            
+            style: TextStyle(
+              color: Colors.blue[700], fontWeight: FontWeight.bold, fontSize: 30),
+        )), 
+        
+        Expanded(
+          child:Text(content, textAlign: TextAlign.left, style: TextStyle(color: Colors.white, fontSize: 20)),
+        ),
+        ],
+      );
   }
 }
+
+// Container(
+//         margin: const EdgeInsets.all(5.0),
+//         padding: const EdgeInsets.all(10.0),
+//         decoration: BoxDecoration(border: Border.all()),
+//         child: Text("id", style: TextStyle(
+//         color: Colors.blue[700], fontWeight: FontWeight.bold),
+//         )
+//       ) 
